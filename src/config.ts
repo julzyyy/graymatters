76b693aeffc84c74864e1ac874c6808afb21cb71
julzyyy <106398998+julzyyy@users.noreply.github.com>
2023-01-25 22:19:12 +1100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ import { chain } from "wagmi";
 
 export const CONTRACT_ADDRESS = "0xD5C33233639B867F3866a0f6f89F8E97DCb08ca0";
 export const CHAIN_ID = chain.mainnet.id;
-export const MAX_MINT_AMOUNT = 99;
+export const MAX_MINT_AMOUNT = 10;
 
 export const COLLECTION_SIZE = 3333;
